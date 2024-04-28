0af145591a2e1a38886bc891149c8516a4dd674f
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-04-28 20:21:05 +0700

    feat: created homeview

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:get/get.dart';
 
@@ -9,16 +10,58 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "SyopShop",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          elevation: 0,
+          leading: const Icon(Icons.shopping_cart_rounded),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.info)),
+          ],
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Welcome to Syopshop',
+                      style: TextStyle(
+                          fontFamily: 'avenir',
+                          fontSize: 32,
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  IconButton(
+                      icon: const Icon(Icons.view_list_rounded),
+                      onPressed: () {}),
+                  IconButton(
+                      icon: const Icon(Icons.grid_view), onPressed: () {}),
+                ],
+              ),
+            ),
+            Expanded(
+child: AlignedGridView.count(
+  itemCount: 100,
+  crossAxisCount: 2,
+
+  crossAxisSpacing: 20,
+  mainAxisSpacing: 20,
+   itemBuilder: (context, index) {
+  return Container(
+    color: Colors.red,
+    height: Get.width / 8,
+    width: 100,
+  );
+},)
+
+                )
+          ],
+        ));
   }
 }
